ad23987e7a46131c33d87277e9c2c3124f9cf7f3
ggehrer <g.gehrer@htl-leonding.ac.at>
2021-03-16 20:09:58 +0100

    Initalize Project Socket

--- SocketCommon/src/main/java/org/htl/leonding/pattern/Observable.java
+++ SocketCommon/src/main/java/org/htl/leonding/pattern/Observable.java
@@ -6,5 +6,12 @@ import java.util.List;
 public abstract class Observable {
     private List<Observer> observers = new ArrayList<>();
 
+    public synchronized void addObserver(Observer observer) {
+        if (observer == null)
+            throw new IllegalArgumentException("observer");
 
+        if (observers.contains(observer) == false) {
+            observers.add(observer);
+        }
+    }
 }
